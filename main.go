518dcebdb9e61cecd8e9ef1723ad470e6296ee9e
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-04-30 16:53:25 +0200

    Switch to prisma for migrations

--- main.go
+++ main.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	database_uri := os.Getenv("DB_SERVER_URI")
-
-	// Setup migrations
-	m, err := migrate.New(
-		"file://db/migrations",
-		database_uri)
-
-	if err != nil {
-		log.Fatalf("Error loading migrations %v", err)
-	}
-
-	// Run migrations
-	if err := m.Up(); err != nil {
-		if err == migrate.ErrNoChange {
-			log.Print("All migrations applied")
-		} else {
-			log.Fatalf("Error migrating up %v", err)
-		}
-	}
-
-	// Setup fiber
 
 	app := fiber.New()
 
